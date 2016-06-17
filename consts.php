a276800774443745a3b45037ba8902baed7d450e
jiawu <poppylin@outlook.com>
2016-06-17 14:41:38 +0800

    consts.php 第一个版本

---
+++ consts.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * 全局常量
+ * @author JiawuLin
+ */
+
+
+//当前请求时间
+define('TIME',      	$_SERVER['REQUEST_TIME']);
+
+//当前UNIX时间戳秒秒数
+define('MTIME', 		microtime(true));
+
+//目录分隔符
+define('DS',			DIRECTORY_SEPARATOR);
+
+
+
+?>
